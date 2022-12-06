1ce7ce01a676db54a63908537edb8914c98d8952
mai.akatsu <debunyankawaii@gmail.com>
2022-12-07 00:13:06 +0900

    jqueryjs修正

--- js/common.js
+++ js/common.js
@@ -20,3 +20,19 @@ if (w > 769) {
     });
   }
 }); 
+
+function fadeIn() {
+  $('.fadeUpTrigger').each(function () {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass('fadeUp');
+    } else {
+      $(this).removeClass('fadeUp');
+    }
+  });
+}
+$(window).scroll(function () {
+  fadeIn();
+});
