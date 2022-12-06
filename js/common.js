412c8b623aec88d873a2fd36f1b46e37b8a291e2
mai.akatsu <debunyankawaii@gmail.com>
2022-12-07 01:20:12 +0900

    '2回目修正'

--- js/common.js
+++ js/common.js
@@ -32,7 +32,48 @@ function fadeIn() {
       $(this).removeClass('fadeUp');
     }
   });
+  $('.fadeTrigger').each(function () {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass('fade');
+    } else {
+      $(this).removeClass('fade');
+    }
+  });
+  $('.fadeRightTrigger').each(function () {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass('fadeRight');
+    } else {
+      $(this).removeClass('fadeRight');
+    }
+  });
+  $('.fadeLeftTrigger').each(function () {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass('fadeLeft');
+    } else {
+      $(this).removeClass('fadeLeft');
+    }
+  });
 }
 $(window).scroll(function () {
   fadeIn();
 });
+$('.lp_clock').slick({
+  autoplaySpeed: 2000,
+  speed: 1200,
+  autoplay: true,
+  infinite: true,
+  slideToShow: 1,
+  slideToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="slick_prev"></div>',
+  nextArrow: '<div class="slick_next"></div>'
+})
